48a862927c39d9d17cdc433dd62bf8be2f8f7391
Imole Dare <imole@kali.kali.com>
2024-10-31 14:25:04 +0100

    re editing solution

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,61 +1,52 @@
 #!/usr/bin/python3
-"""UTF-8 validation module.
-"""
+"""This module defines a function `validUTF8`"""
 
 
 def validUTF8(data):
-    """Checks if a list of integers are valid UTF-8 codepoints.
-    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
+    """ Checks is all data within the list is a valid UTF8 encoding
+
+        UTF8 Format:
+
+        1-byte Sequence: 0xxxxxxx;
+        2-byte Sequence: 110xxxxx 10xxxxxx;
+        3-byte Sequence: 1110xxxx 10xxxxxx 10xxxxxx;
+        4-byte Sequence: 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx;
     """
-    skip = 0
-    n = len(data)
-    for i in range(n):
-        if skip > 0:
-            skip -= 1
-            continue
-        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
-            return False
-        elif data[i] <= 0x7f:
-            skip = 0
-        elif data[i] & 0b11111000 == 0b11110000:
-            # 4-byte utf-8 character encoding
-            span = 4
-            if n - i >= span:
-                next_body = list(map(
-                    lambda x: x & 0b11000000 == 0b10000000,
-                    data[i + 1: i + span],
-                ))
-                if not all(next_body):
-                    return False
-                skip = span - 1
-            else:
+
+    # 0x80 is 10000000
+    # 0xE0 is 11100000
+    # 0xC0 is 11000000
+    # 0xF0 is 11110000
+
+    i = 0
+    while i < len(data):
+        # Check if it's a single-byte character
+        if data[i] & 0x80 == 0x00:
+            i += 1
+        # Check if it's a multi-byte character
+        # 2-byte sequence
+        elif data[i] & 0xE0 == 0xC0:
+            # checks if there is a next term in the sequence and it is
+            # of the format
+            if i + 1 >= len(data) or data[i + 1] & 0xC0 != 0x80:
                 return False
-        elif data[i] & 0b11110000 == 0b11100000:
-            # 3-byte utf-8 character encoding
-            span = 3
-            if n - i >= span:
-                next_body = list(map(
-                    lambda x: x & 0b11000000 == 0b10000000,
-                    data[i + 1: i + span],
-                ))
-                if not all(next_body):
-                    return False
-                skip = span - 1
-            else:
+            i += 2
+        # 3-byte sequence
+        elif data[i] & 0xF0 == 0xE0:
+            # checks if there are two other terms in the sequence and
+            # they are of the format
+            if i + 2 >= len(data) or data[i + 1] & 0xC0 != 0x80 or data[i + 2]\
+                    & 0xC0 != 0x80:
                 return False
-        elif data[i] & 0b11100000 == 0b11000000:
-            # 2-byte utf-8 character encoding
-            span = 2
-            if n - i >= span:
-                next_body = list(map(
-                    lambda x: x & 0b11000000 == 0b10000000,
-                    data[i + 1: i + span],
-                ))
-                if not all(next_body):
-                    return False
-                skip = span - 1
-            else:
+            i += 3
+        # 4-byte sequence
+        elif data[i] & 0xF8 == 0xF0:
+            # checks if there are three other terms in the sequence and
+            # they are of the format
+            if i + 3 >= len(data) or data[i + 1] & 0xC0 != 0x80 or data[i + 2]\
+                    & 0xC0 != 0x80 or data[i + 3] & 0xC0 != 0x80:
                 return False
+            i += 4
         else:
             return False
     return True
